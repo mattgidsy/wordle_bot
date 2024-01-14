670a671989283e8713499ace0a2d42843bc34b61
inquiz <62821667+Inquiz@users.noreply.github.com>
2024-01-13 23:29:00 -0600

    added comments, decriptions and started cleaning up

--- main.py
+++ main.py
@@ -4,20 +4,18 @@ from discord.ext import commands
 from wordlefilter import *
 from validator import *
 
-#global counter
-#counter = 0
+
 
 def run():
-    player = WordleUser()
+ 
+ #create a dictionary to store multiple users 
     user_dict ={}
-    # counter = 0
-    
-    #create a dictionary to handle users and have a command counter
+   
     intents = discord.Intents.all()
     intents.message_content = True
-    
     bot = commands.Bot(command_prefix= "!", intents=intents)
     
+#bot logging
     @bot.event
     async def on_ready():
         print(bot.user)
@@ -68,35 +66,42 @@ def run():
     #         await ctx.send(user_input[i])
     #         i = i+1
     
+#when the bot is being rude    
     @bot.command()
     async def rude(ctx):
         await ctx.send(f"I'm sorry, ~~Dave~~...{ctx.author.name}")
-    
-    #this command can run but it is not storing past guesses
-    @bot.command(user_dict)
+
+ 
+#enter !wordle guess guess_correct_letters example: !wordle slAtE s
+    @bot.command(
+        user_dict,
+        aliases = ['w'],
+        help = "This is the Wordle Helper Bot",
+        description = "To use wordle bot, type your 5 letter guess and the correct letters (yellow letters). \n\nBe sure to CAPITALIZE letters that are in the correct position (green letters) \n\nexample:\n !wordle slAtE s",
+        brief = "This bot helps solve wordles"
+        )
     async def wordle(ctx, guess, guess_cl = ""):
         
+        #creates a user tied to the discord username   
         user_name = ctx.author.name
-        #username = input("Enter your username:\n")
+        
         if user_name not in user_dict:
             user_dict[user_name] = create_new_user(user_name)
         
         player = user_dict[user_name]
         
-        
         if guess == validate_guess(guess):
-            #print("guess validated")
             player.guess = guess
             if guess_cl == "":
-                #print("guess_cl validated")
                 player.guess_cl = guess_cl
                 wordle_filter(player)
                 if len(player.filtered_list) == 0:
-                    await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")
+                    await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")               
                 else:
+                    #batch the lists into 200 word lists to stay under 2k character count on discord
                     for i in range(0, len(player.filtered_list), 200):
                             await ctx.send(f"{player.name}'s list of possible wordle words:")
-                            await ctx.send(player.filtered_list[i:i + 200])                
+                            await ctx.send(player.filtered_list[i:i + 200])                                     
             elif guess_cl == validate_guess_cl(guess_cl):
                     player.guess_cl = guess_cl
                     wordle_filter(player)
@@ -112,12 +117,7 @@ def run():
             await ctx.send(f"{player.name}, there was a problem with your guess's first input. Please remember that I am dumb and can only accept 5 letter words with no special characters.")
             await ctx.send("type !wordle to try again. \nHere's an example: \n!wordle SlAte e")
             
-        #have to batch the lists into 200 word lists to stay under 2k character count on discord
-        
-
-        #await ctx.send(player.filtered_list) 
-        #await ctx.send(player.name)
-    
+#clears the user's saved data  
     @bot.command(user_dict)
     async def clear(ctx):
         user_name = ctx.author.name
@@ -165,17 +165,18 @@ def run():
 #             await ctx.send(f"{player.name}, there was a problem with your guess's first input. Please remember that I am dumb and can only accept 5 letter words with no special characters.")
 #             await ctx.send("type !wordle to try again. \nHere's an example: \n!wordle SlAte e")
 # #testing clear for multiuser            
-    # @bot.command(user_dict)
-    # async def tclear(ctx):
-    #     user_name = "Test_User"
-    #     if user_name in user_dict:
-    #         player = user_dict[user_name]
-    #         player.filtered_list = []
-    #         await ctx.send(f"{user_name}, your wordle list has been cleared.")
-    #     else:
-    #         await ctx.send(f"{user_name}, I don't seem to have a list for you, fam.")
+#     @bot.command(user_dict)
+#     async def tclear(ctx):
+#         user_name = "Test_User"
+#         if user_name in user_dict:
+#             player = user_dict[user_name]
+#             player.filtered_list = []
+#             await ctx.send(f"{user_name}, your wordle list has been cleared.")
+#         else:
+#             await ctx.send(f"{user_name}, I don't seem to have a list for you, fam.")
                     
     bot.run(settings.DISCORD_API_SECRET)
+    
 if __name__ == "__main__":
     run()
 # player = WordleUser()
