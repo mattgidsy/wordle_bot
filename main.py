cf19c96136b3cbbd654624eb58f3f3ae12d5a8e7
inquiz <62821667+Inquiz@users.noreply.github.com>
2024-01-18 23:33:33 -0600

    converted bot to slash commands using hybrid commands

--- main.py
+++ main.py
@@ -26,23 +26,27 @@ def run():
 
     
     #when the bot is being rude    
-    @bot.command()
+    @bot.hybrid_command(
+        help = "when the bot is being rude.",
+        descrption = "when the bot is being rude."
+    )
     async def rude(ctx):
         await ctx.send(f"I'm sorry, ~~Dave~~...{ctx.author.name}") 
                  
     #enter !wordle guess guess_correct_letters example: !wordle slAtE s
-    @bot.command(
+    @bot.hybrid_command(
         user_dict,
         aliases = ['w'],
         help = "This is the Wordle Helper Bot",
-        description = "To use wordle bot, type your 5 letter guess and the correct letters (yellow letters). \n\nBe sure to CAPITALIZE letters that are in the correct position (green letters) \n\nexample:\n !wordle slAtE s",
+        description = "Type your guess: using CAPS for green letters, correct letters = yellow letters",
         brief = "This bot helps solve wordles"
         )
-    async def wordle(ctx, guess, guess_cl = ""):
+    async def wordle(ctx, guess, correct_letters = ""):
         
         #creates a user tied to the discord username   
         user_name = ctx.author.name
         user_id = ctx.author.id
+        guess_cl = correct_letters
         
         if user_id not in user_dict:
             user_dict[user_id] = create_new_user(user_id)
@@ -56,29 +60,33 @@ def run():
                 player.guess_cl = guess_cl
                 wordle_filter(player)
                 if len(player.filtered_list) == 0:
-                    await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")               
+                    await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.", ephemeral=True)               
                 else:
                     #batch the lists into 200 word lists to stay under 2k character count on discord
                     for i in range(0, len(player.filtered_list), 200):
-                            await ctx.send(f"{player.name}'s list of possible wordle words:")
-                            await ctx.send(player.filtered_list[i:i + 200])                                     
+                            await ctx.send(f"{player.name}'s list of possible wordle words:", ephemeral=True)
+                            await ctx.send(f"{player.filtered_list[i:i + 200]}", ephemeral=True)                                     
             elif guess_cl == validate_guess_cl(guess_cl):
                     player.guess_cl = guess_cl
                     wordle_filter(player)
                     if len(player.filtered_list) == 0:
-                        await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")
+                        await ctx.send(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.", ephemeral=True)
                     else:
                         for i in range(0, len(player.filtered_list), 200):
-                                await ctx.send(f"{player.name}'s list of possible wordle words:")
-                                await ctx.send(player.filtered_list[i:i + 200])                    
+                                await ctx.send(f"{player.name}'s list of possible wordle words:", ephemeral=True)
+                                await ctx.send(f"{player.filtered_list[i:i + 200]}", ephemeral=True)                    
             else:
-                await ctx.send(f"{player.name}, there was a problem with your second input. Please remember that I am dumb and can only accept 5 letter words with no special characters (except for an empty field here).")     
+                await ctx.send(f"{player.name}, there was a problem with your second input. Please remember that I am dumb and can only accept 5 letter words with no special characters (except for an empty field here).", ephemeral=True)     
         else:
-            await ctx.send(f"{player.name}, there was a problem with your guess's first input. Please remember that I am dumb and can only accept 5 letter words with no special characters.")
-            await ctx.send("type !wordle to try again. \nHere's an example: \n!wordle SlAte e")
+            await ctx.send(f"{player.name}, there was a problem with your guess's first input. Please remember that I am dumb and can only accept 5 letter words with no special characters.", ephemeral=True)
+            await ctx.send("type /wordle to try again. \nHere's an example: \n!wordle SlAte e", ephemeral=True)
             
     #clears the user's saved data  
-    @bot.command(user_dict)
+    @bot.hybrid_command(
+        user_dict,
+        help = "This clears your old guesses",
+        description = "This clears your old guesses",
+        brief = "This clears your old guesses")
     async def clear(ctx):
         user_name = ctx.author.name
         user_id = ctx.author.id
@@ -86,9 +94,9 @@ def run():
         if user_id in user_dict:
             player = user_dict[user_id]
             player.filtered_list = []
-            await ctx.send(f"{player.name}, your wordle list has been cleared.")
+            await ctx.send(f"{player.name}, your wordle list has been cleared.", ephemeral=True)
         else:
-            await ctx.send(f"{user_name}, I don't seem to have a list for you, fam.")
+            await ctx.send(f"{user_name}, I don't seem to have a list for you, fam.", ephemeral=True)
     
     #sync with discord, use when changes are made to slash commands
     @bot.command(hidden=True)
@@ -99,67 +107,10 @@ def run():
         if user_id == owner_id:
             bot.tree.copy_global_to(guild=settings.GUILDS_ID)
             await bot.tree.sync(guild=settings.GUILDS_ID)
-            await ctx.send(f"{user_name}, I have requested to sync with the hive.")
+            await ctx.send(f"{user_name}, I have requested to sync with the hive.", ephemeral=True)
         else:
             await ctx.send(f"Sorry, {user_name}, you don't have permission to use that command.\n Dad doesn't want to get rate limited.")
-    
-    #### slash command implementation in progress ####
-    
-    #create a userdict global
-    #bot.userdict = {}
-    
-    ##slash command implementation of the wordlebot
-    # @bot.tree.command()
-    # async def wordle(interaction, guesss: str, correct_letters: str ):
             
-    #     #creates a user tied to the discord username   
-    #     user_name = interaction.user.name
-    #     user_id = interaction.user.id 
-    #     guess = guesss
-    #     guess_cl = correct_letters
-    #     print(f"guess:{guess}")
-    #     print(f"guess_cl:{correct_letters}")
-        
-    #     if user_id not in user_dict:
-    #         user_dict[user_id] = create_new_user(user_id)
-        
-    #     player = user_dict[user_id]
-    #     player.name = user_name
-        
-    #     if guess == validate_guess(guess):
-    #         player.guess = guess
-    #         if guess_cl == "":
-    #             player.guess_cl = guess_cl
-    #             wordle_filter(player)
-    #             if len(player.filtered_list) == 0:
-    #                 await interaction.response.send_message(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")               
-    #             else:
-    #                 #batch the lists into 200 word lists to stay under 2k character count on discord
-    #                 for i in range(0, len(player.filtered_list), 200):
-    #                         await interaction.response.send_message(f"{player.name}'s list of possible wordle words:")
-    #                         await interaction.response.send_message(player.filtered_list[i:i + 200])                                     
-    #         elif guess_cl == validate_guess_cl(guess_cl):
-    #                 player.guess_cl = guess_cl
-    #                 wordle_filter(player)
-    #                 if len(player.filtered_list) == 0:
-    #                     await interaction.response.send_message(f"Dear, {player.name}, I'm sorry that I (or you) are at fault here. Your list of possible answers looks empty. Much like my care cup.")
-    #                 else:
-    #                     for i in range(0, len(player.filtered_list), 200):
-    #                             await interaction.response.send_message(f"{player.name}'s list of possible wordle words:")
-    #                             await interaction.response.send_message(player.filtered_list[i:i + 200])                    
-    #         else:
-    #             await interaction.response.send_message(f"{player.name}, there was a problem with your second input. Please remember that I am dumb and can only accept 5 letter words with no special characters (except for an empty field here).")     
-    #     else:
-    #         await interaction.response.send_message(f"{player.name}, there was a problem with your guess's first input. Please remember that I am dumb and can only accept 5 letter words with no special characters.")
-    #         await interaction.response.send_message("type !wordle to try again. \nHere's an example: \n!wordle SlAte e")
-            
-    # ##slash command implementation of rude command
-    # @bot.tree.command()
-    # async def rude(interaction):
-    #     await interaction.response.send_message(f"I'm sorry, ~~Dave~~...{interaction.user.name}") 
-                                       
-    
-
     bot.run(settings.DISCORD_API_SECRET)
     
 if __name__ == "__main__":
